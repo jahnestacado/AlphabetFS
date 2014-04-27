ab296a71be83efa782e541115548e6e55a504ae7
Jahn <ioannis.kiwe@gmail.com>
2014-04-28 00:03:17 +0200

    js/dirTracker.js: Implemented untilIsDone file transfer. There is a bug in the registerFile functionality when no file are present outside AlphabetFS in target dir

--- public_html/js/dirTracker.js
+++ public_html/js/dirTracker.js
@@ -21,20 +21,67 @@ function registerDirectory(targetDirPath) {
 function onFsChange() {
 
     notifications.on('change', function(file, event, path) {
-        var fullpath = path + '/' + file;
-        console.log("Has not being processed ", processedFiles.indexOf(fullpath) === -1);
+        var fullpath = getFullPath(path, file);
+        console.log("Ok to process ", processedFiles.indexOf(fullpath) === -1);
         console.log("Does exist ", fs.existsSync(fullpath));
         console.log("Name ", file);
-
+        // should put it in prototype of array to check if element exists
         if (processedFiles.indexOf(file) === -1 && fs.existsSync(fullpath)) {
             processedFiles.push(fullpath);
             console.log('New file ' + file + 'caught a ' + event + ' event on ' + path);
-            mover.moveToLetterDir(path, file);
 
+            check(path, file);
         }
 
     });
 
+}
+
+function getFullPath(targetDir, fileName) {
+    return   targetDir + '/' + fileName;
+}
+
+
+function check(targetDir, fileName) {
+
+    var path = getFullPath(targetDir, fileName);
+    var oldSize = 0;
+
+    function listenUntilIsDone() {
+
+
+        if (getSize(path) === 4096) {
+            var content = fs.readdirSync(path);
+            var size = content.reduce(function(total, file) {
+                return total + getSize(targetDir + '/' + fileName + '/' + file);
+            }, 0);
+            console.log("Sizeee ", size);
+
+            if (size !== 0 && oldSize === size) {
+
+                mover.moveToLetterDir(targetDir, fileName);
+            }
+            else {
+                oldSize = size;
+                setTimeout(listenUntilIsDone, 2000);
+
+            }
+
+        }
+        else
+            mover.moveToLetterDir(targetDir, fileName);
+    }
+    listenUntilIsDone();
+
+}
+
+
+function getSize(filename) {
+    var exists = fs.existsSync(filename);
+    if (exists) {
+        return fs.statSync(filename).size;
+    }
+    return 0;
 }
 
 
--- public_html/js/mover.js
+++ public_html/js/mover.js
@@ -7,6 +7,8 @@ var numOfPaths = 0;
 
 function startTrackingCheck(targetDirPath) {
     counter++;
+    console.log("Counter ", counter);
+    console.log("NumofPaths" ,numOfPaths);
     if (counter === numOfPaths && numOfPaths !== 0) {
         track.registerDirectory(targetDirPath);
         counter = 0;
@@ -31,15 +33,16 @@ function moveToLetterDir(targetDir, name) {
         var destDir = targetDir + '/' + name.charAt(0).toUpperCase() + '/' + name;
         var isDirectory = fs.lstatSync(originPath).isDirectory();
         if (isDirectory) {
+            //Bug here. It transfers the dir first without content and then deletes it so it doesnt exist to continiou the transfers of the content
             console.log(destDir);
             fs.copy(originPath, destDir, function(error) {
                 if (error)
-                    console.log(error);
+                   return ;
 
 
                 rmdir(originPath, function(error) {
                     if (error)
-                        console.log(error);
+                       return;
                     startTrackingCheck(targetDir);
                 });
 
