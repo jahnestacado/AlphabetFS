8ad04ca3f5c5e5b234504a69b891997f2907c5e2
Jahn <ioannis.kiwe@gmail.com>
2014-07-06 21:52:29 +0200

    server.js: Implemented Socket.io web socket

--- server.js
+++ server.js
@@ -4,34 +4,48 @@ var http = require('http') // http module
         , mover = require('./core/mover.js')
         , alphabetDirectories = require('./core/alphabetDirectories')
         , express = require('express')
+        , app = express()
+        , server = require('http').createServer(app)
+        , io = require('socket.io').listen(server)
         , bodyParser = require('body-parser');
 
-var app = express();
 
-// store the contents of 'index.html' to a buffer
-var html = fs.readFileSync(__dirname + '/public/index.html');
+server.listen(8085);
 
+app.use(express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded()); // to support URL-encoded bodies
-app.set('port', process.env.PORT || 8085);
-app.use(express.static(__dirname + '/public/css'));
 
-app.post('/', function(request, response) {
-    targetDirPath = request.body.text;
-    var content = pathGatherer.getDirContent(targetDirPath);
-    alphabetDirectories.createAlphabetDirs(targetDirPath, content);
-    response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-    response.end(html);
+
+io.sockets.on('connection', function(socket) {
+    console.log("OnConnection ");
+
+    socket.on('path-entry', function(data) {
+        var targetDirPath = data;
+        var content = pathGatherer.getDirContent(targetDirPath);
+        alphabetDirectories.createAlphabetDirs(targetDirPath, content);
+    });
+
+
 });
 
-app.get('/', function(request, response) {
-    // for GET requests, serve up the contents in 'index.html'       
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end(html);
-});
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+
+//app.post('/', function(request, response) {
+
+//    response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+//    response.end(html);
+//});
+//
+//app.get('/', function(request, response) {
+//    // for GET requests, serve up the contents in 'index.html'       
+//    response.writeHead(200, {'Content-Type': 'text/html'});
+//    response.end(html);
+//});
+//
+//http.createServer(app).listen(app.get('port'), function() {
+//    console.log('Express server listening on port ' + app.get('port'));
+//});
 
 
