b5826d1fadbea8217fb40d81302d0c1362d5006e
Jahn <ioannis.kiwe@gmail.com>
2014-08-31 20:04:43 +0200

    db/db-utils.js: bus.socket.emitUIEvent should be call always

--- db/db-utils.js
+++ db/db-utils.js
@@ -7,8 +7,8 @@ bus.onEvent("storePathNUpdateUI", function(path) {
         if (registeredPaths.indexOf(path) === -1) {
             registeredPaths.push(path);
             db.save("abc-fs", "registered-paths", registeredPaths);
-            bus.socket.emitUIEvent({event: "register-path", path: path});
         }
+            bus.socket.emitUIEvent({event: "register-path", path: path});
     });
 }).registerLocation(__filename);
 
