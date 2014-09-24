f2644cb027b1ec73ca2f0b146bb293552b841213
Jahn <ioannis.kiwe@gmail.com>
2014-09-24 21:55:22 +0200

    test/core/alphabetDirectories.test.js: Wrote tests for test/core/alphabetDirectories module

---
+++ test/core/alphabetDirectories.test.js
@@ -0,0 +1,85 @@
+var assert = require('assert');
+var abcDirs = require('./../../core/alphabetDirectories.js');
+var pathGatherer = require("./../../core/pathGatherer.js");
+var sinon = require('sinon');
+var bus = require('hermes-bus');
+var utils = require("./../utils.js");
+var fs = require('fs');
+var rmdir = require('rimraf');
+var testingFolder = "testing-workspace";
+
+describe('starting integration tests for alphabetDirectories module', function() {
+    bus.subscribeEventsFrom('./core/mover.js');
+
+    describe('invoke initiateStructure', function() {
+        var content = {};
+        var abcDirTestingPaths = abcDirs.getAlphabetFsStructure().map(function(absFsDir) {
+            return testingFolder + '/' + absFsDir;
+        });
+
+        bus.core.emitMoveToAlphabetDirs = sinon.spy();
+
+        abcDirs.isAlphabetFSDirectory = function() {
+            return false
+        };
+
+        before(function(done) {
+            abcDirs.initiateStructure(testingFolder, content, done);
+        });
+
+        it('should invoke  bus.core.emitMoveToAlphabetDirs', function() {
+            assert(bus.core.emitMoveToAlphabetDirs.calledOnce);
+        });
+
+        describe('gather targeted directory content', function() {
+
+            var testingDirContent;
+            before(function() {
+                testingDirContent = pathGatherer.getDirContent(testingFolder);
+            });
+
+            it('should have created alphabetFs structure', function() {
+                assert(utils.areArrayContentsEqual(abcDirTestingPaths, testingDirContent.dirPaths));
+            });
+
+        });
+
+        describe('create file x-file.txt in X directory', function() {
+
+            var x_filePath = testingFolder + "/X" + "/x-file.txt";
+            before(function() {
+                fs.openSync(x_filePath, 'w')
+            });
+
+            it('should have created x-file inside X directory', function() {
+                assert(fs.existsSync(x_filePath));
+            });
+
+            describe('invoke again initiateStructure ', function() {
+
+                var testingDirContent;
+                before(function(done) {
+                    abcDirs.initiateStructure(testingFolder, content, done);
+                    testingDirContent = pathGatherer.getDirContent(testingFolder);
+                });
+
+                it('should have valid alphabetFs structure', function() {
+                    assert(utils.areArrayContentsEqual(abcDirTestingPaths, testingDirContent.dirPaths));
+                });
+
+                it('should not overwrite content of X directory', function() {
+                    assert(fs.existsSync(x_filePath));
+                });
+
+            });
+
+        });
+
+    });
+
+    after(function() {
+        rmdir.sync(testingFolder);
+    });
+
+
+});
