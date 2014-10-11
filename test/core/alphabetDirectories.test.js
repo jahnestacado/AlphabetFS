ba2f0bc2d7f036b466038223f1ce8616337766c4
Jahn <ioannis.kiwe@gmail.com>
2014-10-11 20:49:30 +0200

    test/core/alphabetDirectories.test.js: Refactored test to use stubs/spies for external dependencies

--- test/core/alphabetDirectories.test.js
+++ test/core/alphabetDirectories.test.js
@@ -1,89 +1,83 @@
 var assert = require('assert');
 var abcDirs = require('./../../core/alphabetDirectories.js');
-var pathGatherer = require("./../../core/pathGatherer.js");
 var sinon = require('sinon');
 var bus = require('hermes-bus');
-var utils = require("./../utils.js");
 var fs = require('fs');
-var rmdir = require('rimraf');
-var testingFolder = "testing-workspace";
-
-var old = abcDirs.isAlphabetFSDirectory;
+var mkdirp = require('mkdirp');
 
 describe('#################### Starting integration tests for alphabetDirectories module', function() {
+    
     bus.subscribeEventsFrom('./core/mover.js');
+    var testingFolder = "testing-workspace";
+    var sandbox = sinon.sandbox.create();
+    var abcDirTestingPaths = abcDirs.getAlphabetFsStructure().map(function(abcFsDir) {
+        return testingFolder + '/' + abcFsDir;
+    });
 
-    var isAlphabetFSDirectoryStub = sinon.stub(abcDirs, 'isAlphabetFSDirectory', function() {
-        return false;
+    var fsExistsStub;
+    var mkdirpStub;
+    var emitMoveToAlphabetDirsSpy;
+
+    before('stubbing relevant function', function() {
+        fsExistsStub = sandbox.stub(fs, 'existsSync');
+        mkdirpStub = sandbox.stub(mkdirp, "sync");
+        emitMoveToAlphabetDirsSpy = bus.core.emitMoveToAlphabetDirs = sandbox.spy();
+        sandbox.stub(abcDirs, 'isAlphabetFSDirectory', function() {
+            return false;
+        });
     });
 
     describe('invoke initiateStructure', function() {
         var content = {};
-        var abcDirTestingPaths = abcDirs.getAlphabetFsStructure().map(function(absFsDir) {
-            return testingFolder + '/' + absFsDir;
-        });
-
-        bus.core.emitMoveToAlphabetDirs = sinon.spy();
 
         before(function(done) {
+            abcDirTestingPaths.forEach(function(element) {
+                fsExistsStub.withArgs(element)
+                        .onCall(0).returns(false)
+                        .onCall(1).returns(true);
+                mkdirpStub.withArgs(element).returns(true);
+            });
             abcDirs.initiateStructure(testingFolder, content, done);
         });
 
+        abcDirTestingPaths.forEach(function(element) {
+            it('should have been called with argument ' + element, function() {
+                assert(mkdirpStub.withArgs(element).calledOnce);
+            });
+        });
+
+        it('should have invoked mkdirp 27 times', function() {
+            assert.equal(mkdirpStub.callCount, 27);
+        });
+
         it('should invoke  bus.core.emitMoveToAlphabetDirs', function() {
-            assert(bus.core.emitMoveToAlphabetDirs.calledOnce);
+            assert(emitMoveToAlphabetDirsSpy.calledOnce);
         });
 
-        describe('gather targeted directory content', function() {
+        describe('invoke again initiateStructure while abcStructure is already created', function() {
 
-            var testingDirContent;
-            before(function() {
-                testingDirContent = pathGatherer.getDirContent(testingFolder);
+            before(function(done) {
+                abcDirs.initiateStructure(testingFolder, content, done);
             });
 
-            it('should have created alphabetFs structure', function() {
-                assert(utils.areArrayContentsEqual(abcDirTestingPaths, testingDirContent.dirPaths));
+            it('should not invoke mkdirp again', function() {
+                assert.equal(mkdirpStub.callCount, 27);
             });
 
+            it('should invoke  bus.core.emitMoveToAlphabetDirs again', function() {
+                assert(emitMoveToAlphabetDirsSpy.calledTwice);
+            });
         });
 
-        describe('create file x-file.txt in X directory', function() {
-
-            var x_filePath = testingFolder + "/X" + "/x-file.txt";
-            before(function() {
-                fs.openSync(x_filePath, 'w')
-            });
-
-            it('should have created x-file inside X directory', function() {
-                assert(fs.existsSync(x_filePath));
-            });
-
-            describe('invoke again initiateStructure ', function() {
-
-                var testingDirContent;
-                before(function(done) {
-                    abcDirs.initiateStructure(testingFolder, content, done);
-                    testingDirContent = pathGatherer.getDirContent(testingFolder);
-                });
-
-                it('should have valid alphabetFs structure', function() {
-                    assert(utils.areArrayContentsEqual(abcDirTestingPaths, testingDirContent.dirPaths));
-                });
-
-                it('should not overwrite content of X directory', function() {
-                    assert(fs.existsSync(x_filePath));
-                });
-
-            });
-
+        after(function() {
+            mkdirpStub.reset();
+            emitMoveToAlphabetDirsSpy.reset();
         });
 
     });
 
     after(function() {
-        rmdir.sync(testingFolder);
-        isAlphabetFSDirectoryStub.restore();
+        sandbox.restore();
         console.log("  ------------------------------ End of integration tests for alphabetDirectories module\n")
     });
-
-
 });
